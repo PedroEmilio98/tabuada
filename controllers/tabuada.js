076bd57aa3ae599a520f39b27dbb5829f6afe1f0
Pedro Emilio <pedroemiliokuhn@gmail.com>
2021-10-11 11:52:45 -0300

    testes unitarios e cobertura de codigo vercel.json

--- controllers/tabuada.js
+++ controllers/tabuada.js
@@ -1,9 +1,23 @@
 const list = (req, res) => {
-    res.render('tabuada/list')
+    let tabuadas = []
+    for (let i = 1; i <= 100; i++) {
+        tabuadas.push(i)
+    }
+    res.render('tabuada/list', { tabuadas })
 }
 const tabuada = (req, res) => {
+    let tabuadas = []
+    const num = req.params.num
+    for (let i = 1; i <= 100; i++) {
+        tabuadas.push({
+            num,
+            i,
+            resultado: num * i
+        })
+    }
     res.render('tabuada/tabuada', {
-        num: req.params.num
+        num: req.params.num,
+        tabuadas
     })
 }
 
